28a0f1a30f8f2e39e4f9ac2bf4863368682f8232
Nicholas Spolti De Souza <nicholasspoltidesouza@gmail.com>
2023-05-30 00:21:35 -0300

    abacaxi

---
+++ demo/demo/src/main/java/com/T5Verival/demo/T5VerivalApplication.java
@@ -0,0 +1,13 @@
+package com.T5Verival.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T5VerivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T5VerivalApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/T5Verival/demo/T5VerivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T5Verival.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T5VerivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
